2230ea4acf345884dc024c9b0270693b5cf7d728
devjsonstringify <dev.json.stringify@gmail.com>
2024-07-26 18:52:26 -0400

    Add dummy crud

--- main.py
+++ main.py
@@ -4,12 +4,40 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    """_summary_
-    """
+    """_summary_"""
     return {"Hello": "World"}
+
+@app.get("/")
+def root():
+    """_summary_"""
+    return "todooo"
+
+@app.post("/todo")
+def create_todo():
+    """_summary_"""
+    return "create todo item"
+
+@app.get("/todo/{id}")
+def read_todo(id: int):
+    """_summary_"""
+    return "read todo item with id {id}"
+
+@app.put("/todo/{id}")
+def update_todo(id: int):
+    """_summary_"""
+    return "update todo item with id {id}"
+
+@app.delete("/todo/{id}")
+def delete_todo(id: int):
+    """_summary_"""
+    return "delete todo item with id {id}"
+
+@app.get("/todo")
+def read_todo_list():
+    """_summary_"""
+    return "read todo list"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
